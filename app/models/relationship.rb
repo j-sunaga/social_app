21b31ff112eb83bc86266aed329140b41bfceebd
sunaga <sunaga@gmail.com>
2019-12-15 11:49:32 +0700

    optionalの追記

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -1,4 +1,4 @@
 class Relationship < ApplicationRecord
   belongs_to :user, class_name: "User"
-  belongs_to :friend, class_name: "User" 
+  belongs_to :friend, class_name: "User",optinal: true
 end
