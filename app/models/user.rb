efabf8238f25e8ae7c5de3aa2ae27cf2edf6150a
sunaga <sunaga@gmail.com>
2019-12-15 11:43:12 +0700

    userモデルとrelationshipモデルの編集

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,26 @@ class User < ApplicationRecord
 
   #アソシエーション
   has_many :timelines
-  
+
+  has_many :active_relationships, class_name: "Relationship",
+                                  foreign_key: "user_id",
+                                  dependent: :destroy
+
+  has_many :passive_relationships, class_name: "Relationship",
+                                  foreign_key: "friend_id",
+                                  dependent: :destroy
+
+  has_many :following, through: :active_relationships,source: :user
+  has_many :followers, through: :passive_relationships,source: :friend
+
   #Validationの追加
   validates :name, presence: true, length: {maximum: 30}
   validates :email, presence: true, length: {maximum: 255},format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
   before_validation { email.downcase! }
   has_secure_password
   validates :password, length: { minimum: 6 }
+
+  def follow(other_user)
+    following << other_user
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_14_062641) do
+ActiveRecord::Schema.define(version: 2019_12_15_041504) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "relationships", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "friend_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["friend_id"], name: "index_relationships_on_friend_id"
+    t.index ["user_id", "friend_id"], name: "index_relationships_on_user_id_and_friend_id", unique: true
+    t.index ["user_id"], name: "index_relationships_on_user_id"
+  end
 
   create_table "timelines", force: :cascade do |t|
     t.string "title"
