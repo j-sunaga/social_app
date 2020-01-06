006e38de8d10acf42f542ae9995fb9d92fb20bcf
sunaga <sunaga@gmail.com>
2020-01-06 21:04:46 +0700

    会員登録時にログイン認証をスキップするように変更。会員登録のビューの変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :user_find, only: [:show]
+  skip_before_action :login_required, only: [:new,:create]
 
   def new
      @user = User.new
