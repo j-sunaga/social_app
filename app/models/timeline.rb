918c1d7048608cefedc555370b3f6edac17f7bda
sunaga <sunaga@gmail.com>
2020-01-11 11:51:28 +0700

    timelineモデルのタイトル列のバリデーションの追加

--- app/models/timeline.rb
+++ app/models/timeline.rb
@@ -1,5 +1,6 @@
 class Timeline < ApplicationRecord
   belongs_to :user
+  validates :title, presence: true, length: { in: 1..20 }
   validates :content, presence: true, length: { in: 1..200 }
   mount_uploader :image, ImageUploader
 end
