2746da5f3165b28db6bd4c0df323bb050fc6bdfe
sunaga <sunaga@gmail.com>
2020-01-15 19:46:15 +0700

    scatfold時の不要なコメントを削除

--- app/controllers/timelines_controller.rb
+++ app/controllers/timelines_controller.rb
@@ -1,18 +1,15 @@
 class TimelinesController < ApplicationController
   before_action :set_timeline, only: [:show, :edit, :update, :destroy]
   before_action :user_check, only: [:edit,:update,:destroy]
-  # GET /timelines
-  # GET /timelines.json
+
   def index
     @timelines = Timeline.all.order(created_at: "DESC")
     @users = User.all
   end
 
-  # GET /timelines/1
   def show
   end
 
-  # GET /timelines/new
   def new
     if params[:back]
       @timeline = current_user.timelines.build(timeline_params)
@@ -21,45 +18,29 @@ class TimelinesController < ApplicationController
     end
   end
 
-  # GET /timelines/1/edit
   def edit
   end
 
-  # POST /timelines
   def create
     @timeline = current_user.timelines.build(timeline_params)
-
-    respond_to do |format|
       if @timeline.save
-        format.html { redirect_to @timeline, notice: 'タイムラインの投稿が完了しました。' }
-        format.json { render :show, status: :created, location: @timeline }
+        redirect_to timelines_path, notice: "タイムラインを投稿しました！"
       else
-        format.html { render :new }
-        format.json { render json: @timeline.errors, status: :unprocessable_entity }
+        render action: :new
       end
-    end
   end
 
-  # PATCH/PUT /timelines/1
   def update
-    respond_to do |format|
       if @timeline.update(timeline_params)
-        format.html { redirect_to @timeline, notice: 'タイムラインのアップデートが完了しました。' }
-        format.json { render :show, status: :ok, location: @timeline }
+        redirect_to timelines_path, notice: "タイムラインを編集しました！"
       else
-        format.html { render :edit }
-        format.json { render json: @timeline.errors, status: :unprocessable_entity }
+        render :edit
       end
-    end
   end
 
-  # DELETE /timelines/1
   def destroy
     @timeline.destroy
-    respond_to do |format|
-      format.html { redirect_to timelines_url, notice: 'タイムラインが削除されました。' }
-      format.json { head :no_content }
-    end
+    redirect_to timelines_path, notice:"タイムラインを削除しました！"
   end
 
   def confirm
@@ -68,12 +49,11 @@ class TimelinesController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
+
     def set_timeline
       @timeline = Timeline.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def timeline_params
       params.require(:timeline).permit(:title, :content, :image, :user_id,:image_cache)
     end
