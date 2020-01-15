6ae7d37746eef2fac9373311678f34fe3fcc0faa
sunaga <sunaga@gmail.com>
2020-01-15 19:48:10 +0700

    新規登録時にログイン画面にリダイレクトできるように変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
   def create
     @user= User.new(user_params)
     if @user.save
-      redirect_to user_path(@user.id)
+      redirect_to new_session_path, notice: "会員登録が完了しました。"
     else
       render 'new'
     end
@@ -29,7 +29,7 @@ class UsersController < ApplicationController
       else
         render :edit
       end
-    end
+  end
 
 
   private
