22b2a92d67ca36ad89f82d9d302c2a22e9cc01a5
junichi sunaga <1224577blue@gmail.com>
2020-01-18 05:35:06 +0000

    会員登録後に自動ログインできるように変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,8 @@ class UsersController < ApplicationController
   def create
     @user= User.new(user_params)
     if @user.save
-      redirect_to new_session_path, notice: "会員登録が完了しました。"
+      session[:user_id]=@user.id
+      redirect_to timelines_path, notice: "会員登録が完了しました。"
     else
       render 'new'
     end
